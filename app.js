8af14371c992b90d2560cd7b4c6f5d8d58fea847
ErikJJ <jive931221@gmail.com>
2023-01-24 23:53:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+
+const formulario = document.querySelector('#formulario');
+const respuesta = document.querySelector('#respuesta');
+
+
+/*Funcion para sacar los datos del Formulario con FormData (ve la leccion anterior)*/
+
+const getData = () => {
+    const datos = new FormData(formulario);
+    const datosProcesados = Object.fromEntries(datos.entries());
+    //formulario.reset();
+    return datosProcesados;
+}
+
+
+
+/*Funcion para colocar los datos en el Servidor */
+
+const postData = async () => {
+
+    /*Crea un objeto con la informacion del formulario*/
+    const newUser = getData();
+
+    try {
+        const response = await fetch('http://localhost:3000/users', {
+            /*especifica el metodo que se va a usar*/
+            method: 'POST',
+            /*especifica el tipo de informacion (JSON)*/
+            headers: { 'Content-Type': 'application/json' },
+            /*coloca la informacion en el formato JSON */
+            body: JSON.stringify(newUser)
+        });
+
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+
+            /* Codigo a ejecutarse con la respuesta*/
+
+            const { nombre, apellido1, apellido2 } = jsonResponse;
+
+            respuesta.innerHTML =
+                `<ul> 
+             ¡Exito! Se guardó la siguiente información:
+            <li>${nombre}</li> 
+            <li>${apellido1}</li> 
+            <li>${apellido2}</li>
+          </ul>`
+
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation');
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        } else {
+            postData()
+        }
+        form.classList.add('was-validated')
+
+    })
+})
+
+
